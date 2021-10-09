ca886b656c9c4079338d6c5d231ae0798e972d1a
Agisza <arkira00@gmail.com>
2021-10-09 11:53:50 +0700

    Ex8

---
+++ Exercise8_Kritsada_n.py
@@ -0,0 +1,33 @@
+print("------Login-------")
+User_Name = input("Username :")
+User_Pass = input("Password :")
+if User_Name == "admin" and User_Pass == "1234" :
+    print("-----Welcome Please Select Item-----")
+    print("1. Ipad ")
+    print("2. Ipad Pro ")
+    print("3. Iphone ")
+    print("4. Iphone Pro")
+    print("5. Iphone Pro Max")
+    input_key = int(input("Select : "))
+    if input_key == 1   :
+        item_name ="Ipad"
+        Item_price = 10000
+    elif input_key == 2 :
+        item_name ="Ipad Pro"
+        Item_price = 25000
+    elif input_key == 3 :
+        item_name ="Iphone"
+        Item_price = 29000
+    elif input_key == 4 :
+        item_name ="Iphone Pro"
+        Item_price = 35000
+    else :
+        tem_name ="Iphone Pro Max"
+        Item_price = 45000
+    print("You select :",item_name)
+    print("Price : ",Item_price," Baht")
+    Item_Qty = int(input("How many pieces : "))
+    print("price summary")
+    print("You Buy : ",item_name," Qty : ",Item_Qty," Price : ",Item_Qty*Item_price)
+else :
+    print("Login Error")
